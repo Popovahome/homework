ec19f9cfd6cc16dacdb53f80f11baff7e986cb79
Popovahome <mashagritsenko5@gmail.com>
2020-10-14 12:39:01 +0300

    Homework14.10

--- Loops/src/loop1/Main.java
+++ Loops/src/loop1/Main.java
@@ -7,25 +7,25 @@ public class Main {
     public static void main(String[] args) {
 
         //вывод таблицы умножения через While
-        /*System.out.println("ВВедите параметр таблицы умножения ");
+        System.out.println("ВВедите параметр таблицы умножения ");
         int a = tt.nextInt();
         int n = 1;
         while (n<=10){
             System.out.println( a + "  *  " + n + "  =  " + a*n);
             n++;
-        }*/
+        }
 
         //Bывод таблицы умножения через Do
-        /*System.out.println("ВВедите параметр таблицы умножения ");
+        System.out.println("ВВедите параметр таблицы умножения ");
         int a1 = tt.nextInt();
         int n1= 1;
         do{
             System.out.println( a1 + "  *  " + n1 + "  =  " + a1*n1);
             n1++;
         }
-        while(n1<=10);*/
+        while(n1<=10);
 
-        //ывод таблицы через For
+        //Вывод таблицы через For
         System.out.println("ВВедите параметр таблицы умножения ");
         int a2 = tt.nextInt();
         for (int n2 = 1; n2<= 10; n2 ++){
---
+++ Loops/src/loop4/Main.java
@@ -0,0 +1,10 @@
+package loop4;
+
+public class Main {
+    public static void main(String[] args) {
+        int a1 = 453;
+        for (int a = 1; a<= 10; a ++){
+            System.out.println( a + "  *  " + a1 + "  =  " + a*a1 +"    грамм в " + a + " фунте/ах");
+        }
+    }
+}
